f4c089e7e00184e989f35ff520c1a188ec5065b0
yaskevich <alyaxey.yaskevich@gmail.com>
2020-11-12 01:48:45 +0300

    typing (-vars)

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps : [
       {
-        name: "vectores",
+        name: "vector",
         script: "./index.js",
         watch: false,
         instance_var: 'INSTANCE_ID',
--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 'use strict';
 const express = require('express');
-var urljoin = require('url-join');
+const urljoin = require('url-join');
 const app = express();
 const fs = require('fs')  
 const path = require('path')  
@@ -11,13 +11,12 @@ const loki = require('lokijs')
 const port = 3434;
 app.use(express.static('maps'))
 // app.use(morgan('combined'));
-var db = new loki('vectores.db', {
+const db = new loki('vectores.db', {
     autoload: true,
     autoloadCallback : databaseInitialize,
     autosave: true, 
     autosaveInterval: 1000
 });
-
 
 const static_path = path.join(__dirname, '..', 'static');
 
@@ -25,7 +24,7 @@ app.use(express.static(static_path))
 app.use(express.static('./node_modules/mini.css/dist'))
 // implement the autoloadback referenced in loki constructor
 function databaseInitialize() {
-  var entries = db.getCollection("entries");
+  let entries = db.getCollection("entries");
   if (entries === null) {
     entries = db.addCollection('entries', { indices: ['word', 'wordpos'] });
   }
@@ -35,7 +34,7 @@ function databaseInitialize() {
 
 // example method with any bootstrap logic to run after database initialized
 function runProgramLogic() {
-  var entryCount = db.getCollection("entries").count();
+  const entryCount = db.getCollection("entries").count();
   console.log("number of entries in database : " + entryCount);
 }
 
@@ -46,12 +45,11 @@ function(req, res){
 res.sendFile(path.join(static_path, 'index.html'));
 }); 
 
-
 app.get('/last',
 	function(req, res){
-		var count  = db.getCollection("entries").count()
+		const count  = db.getCollection("entries").count()
 		if(count) {
-			var dbres = db.getCollection("entries").get(count);
+			const dbres = db.getCollection("entries").get(count);
 			console.log("last");
 			// console.log(dbres);
 			res.send(dbres.data);
@@ -70,7 +68,6 @@ app.get('/syn',
 		let curLang  = userLangs?userLangs[0].substring(0,2): "en";
 		console.log("user language:", curLang);
 		
-		
 		let word  = "день";
 		if (req.query.hasOwnProperty("word")){
 			let wordIn = req.query["word"]; 
@@ -80,7 +77,7 @@ app.get('/syn',
 			}
 		}
 		//https://github.com/techfort/LokiJS/wiki
-		var dbres = db.getCollection("entries").find({ word :word });
+		const dbres = db.getCollection("entries").find({ word :word });
 		// console.log("db", word, dbres.length);
 		
 		// let model = "ruscorpora_upos_cbow_300_20_2019";
@@ -98,14 +95,9 @@ app.get('/syn',
 	} else {
 		axios.get(newurl)
 		  .then(response => {
-			  //let obj = response.data;
-			  //obj = obj[Object.keys(obj)[0]];
-			  //obj = obj[Object.keys(obj)[0]];
-			// console.log(obj);
 			console.log(response.data);
 			db.getCollection("entries").insert( { word : word, wordpos: word, data: response.data } );
 			res.send(response.data);
-			
 			
 			// axios.all([
 			  // axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2017-08-03'),
@@ -125,7 +117,6 @@ app.get('/syn',
 	}
 	// res.send(JSON.stringify({value: newurl}));
 }); 
-
 
 app.listen(port, () => {
     console.log('\nListening to port ' + port);
