4abb3d02326b26eec2a655f3c7e3f88d05e84750
yaskevich <alyaxey.yaskevich@gmail.com>
2021-10-27 23:45:15 +0300

    create new client in Vue (skeleton)

---
+++ client/src/components/HelloWorld.vue
@@ -0,0 +1,31 @@
+<template>
+  <h1>{{ msg }}</h1>
+
+  <p>
+    Test page
+  </p>
+
+  <Button @click="state.count++" label="Count"></Button>
+  <h6>{{ state.count }}</h6>
+
+  <p>
+    Edit
+    <code>components/HelloWorld.vue</code> to test hot module replacement.
+  </p>
+</template>
+
+<script setup>
+import { reactive } from 'vue'
+
+defineProps({
+  msg: String
+})
+
+const state = reactive({ count: 0 })
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
