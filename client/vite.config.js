01d80ad47ee0531246cb3973a086a1c5851db025
yaskevich <alyaxey.yaskevich@gmail.com>
2021-10-30 16:43:14 +0300

    update Vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,5 +4,16 @@ import vue from '@vitejs/plugin-vue'
  * @type {import('vite').UserConfig}
  */
 export default {
-  plugins: [vue()]
+  plugins: [vue()],
+  server: {
+    port: 3070,
+    proxy: {
+     '/api': {
+       target: 'http://localhost:3071',
+       changeOrigin: true,
+       secure: false,
+       ws: true,
+     }
+   }
+  }
 }
