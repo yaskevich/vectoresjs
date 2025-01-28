fc7c02717cbf3d380967a673f530c02158701bdc
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2025-01-28 16:56:24 +0100

    refactor

--- server/index.js
+++ server/index.js
@@ -1,122 +1,76 @@
-'use strict';
-const express = require('express');
-const urljoin = require('url-join');
-const app = express();
-const fs = require('fs')  
-const path = require('path')  
-const axios = require('axios')
-const bodyParser = require('body-parser');
-const loki = require('lokijs')
+import express from 'express';
+import path from 'path';
+import loki from 'lokijs';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const port = 3434;
-app.use(express.static('maps'))
-// app.use(morgan('combined'));
-const db = new loki('vectores.db', {
-    autoload: true,
-    autoloadCallback : databaseInitialize,
-    autosave: true, 
-    autosaveInterval: 1000
+const model = 'ruscorpora_upos_cbow_300_20_2019';
+// let model = 'tayga_upos_skipgram_300_2_2019';
+const format = 'json';
+const colName = 'entries';
+
+const autoloadCallback = () => {
+    db.getCollection(colName) || db.addCollection(colName, { indices: ['word', 'wordpos'] });
+    console.log('Records:', db.getCollection(colName).count());
+};
+
+const cssPath = path.join(__dirname, 'node_modules', 'mini.css', 'dist');
+const staticPath = path.join(__dirname, '..', 'static');
+const dbPath = path.join(__dirname, 'vectores.db');
+
+const db = new loki(dbPath, {
+    autoload: true, autoloadCallback, autosave: true, autosaveInterval: 1000
 });
 
-const static_path = path.join(__dirname, '..', 'static');
+const app = express();
+app.use(express.static(staticPath));
+app.use(express.static(cssPath));
 
-app.use(express.static(static_path))
-app.use(express.static('./node_modules/mini.css/dist'))
-// implement the autoloadback referenced in loki constructor
-function databaseInitialize() {
-  let entries = db.getCollection("entries");
-  if (entries === null) {
-    entries = db.addCollection('entries', { indices: ['word', 'wordpos'] });
-  }
-  // kick off any program logic or start listening to external events
-  runProgramLogic();
-}
-
-// example method with any bootstrap logic to run after database initialized
-function runProgramLogic() {
-  const entryCount = db.getCollection("entries").count();
-  console.log("number of entries in database : " + entryCount);
-}
-
-app.get('/',
-// require('connect-ensure-login').ensureLoggedIn(),
-function(req, res){
-	// logger.log("info", req.headers['user-agent']);
-res.sendFile(path.join(static_path, 'index.html'));
-}); 
+app.get('/', (req, res) => res.sendFile(path.join(staticPath, 'index.html')));
 
 app.get('/last',
-	function(req, res){
-		const count  = db.getCollection("entries").count()
-		if(count) {
-			const dbres = db.getCollection("entries").get(count);
-			console.log("last");
-			// console.log(dbres);
-			res.send(dbres.data);
-		} else {
-			res.sendFile(path.join(static_path, 'def.json'));
-		}
-}); 
+    (req, res) => {
+        const count = db.getCollection(colName).count()
+        if (count) {
+            const dbres = db.getCollection(colName).get(count);
+            // console.log("last");
+            // console.log(dbres);
+            res.send(dbres.data);
+        } else {
+            res.sendFile(path.join(staticPath, 'def.json'));
+        }
+    });
 
-app.get('/def', function(req, res){
-	res.sendFile(path.join(static_path, 'data.json'));
-}); 
+app.get('/def', (req, res) => res.sendFile(path.join(staticPath, 'data.json')));
 
-app.get('/syn',
-	function(req, res){
-		let userLangs = req.acceptsLanguages();
-		let curLang  = userLangs?userLangs[0].substring(0,2): "en";
-		console.log("user language:", curLang);
-		
-		let word  = "день";
-		if (req.query.hasOwnProperty("word")){
-			let wordIn = req.query["word"]; 
-			if (/^[А-ЯЁа-яё]+[A-Za-z_]*$/.test(wordIn) && wordIn){
-				console.log(wordIn);
-				word = wordIn;
-			}
-		}
-		//https://github.com/techfort/LokiJS/wiki
-		const dbres = db.getCollection("entries").find({ word :word });
-		// console.log("db", word, dbres.length);
-		
-		const model = "ruscorpora_upos_cbow_300_20_2019";
-		// let model = "tayga_upos_skipgram_300_2_2019";
-		const format  = "json";
-		const newurl =  urljoin('https://rusvectores.org', model, encodeURIComponent(word), 'api', format)
-		
-	// logger.log("info", req.headers['user-agent']);
-	
-	if (dbres.length){
-		//res.send({error});
-		//console.log("in db", dbres.shift().data);
-		console.log("SERVED FROM CACHE!");
-		res.send(dbres.shift().data);
-	} else {
-		axios.get(newurl)
-		  .then(response => {
-			console.log(response.data);
-			db.getCollection("entries").insert( { word : word, wordpos: word, data: response.data } );
-			res.send(response.data);
-			
-			// axios.all([
-			  // axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2017-08-03'),
-			  // axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2017-08-02')
-			// ]).then(axios.spread((response1, response2) => {
-			  // console.log(response1.data.url);
-			  // console.log(response2.data.url);
-			// })).catch(error => {
-			  // console.log(error);
-			// });
-			
-		  })
-		  .catch(error => {
-			console.log(error);
-			res.send({error});
-		  });
-	}
-	// res.send(JSON.stringify({value: newurl}));
-}); 
-
-app.listen(port, () => {
-    console.log('\nListening to port ' + port);
+app.get('/syn', async (req, res) => {
+    // let userLangs = req.acceptsLanguages();
+    // let curLang = userLangs ? userLangs[0].substring(0, 2) : "en";
+    // console.log("user language:", curLang);
+    let word = (req.query?.word && /^[А-ЯЁа-яё]+[A-Za-z_]*$/.test(req.query.word)) ? req.query.word : 'проблема';
+    const url = ['https://rusvectores.org', model, encodeURIComponent(word), 'api', format].join('/');
+    //https://github.com/techfort/LokiJS/wiki
+    const dbres = db.getCollection(colName).find({ word });
+    console.log(word, url);
+    if (dbres.length) {
+        // console.log('SERVED FROM CACHE!');
+        res.send(dbres.shift().data);
+    } else {
+        try {
+            const response = await fetch(url);
+            if (!response.ok) {
+                console.log('ERROR', word);
+                throw new Error(`Response status: ${response.status}`);
+            }
+            const data = await response.json();
+            // console.log(data);
+            db.getCollection(colName).insert({ word, wordpos: word, data });
+            res.json(data);
+        } catch (error) {
+            console.error(error.message);
+        }
+    }
 });
+
+app.listen(port, () => console.log('Listening to port ' + port));
